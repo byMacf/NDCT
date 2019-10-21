c03334c12948b49d68cfa1304e76fb41c94b74bd
Dom Macfarlane <dominicmacfarlane1@hotmail.com>
2019-10-21 13:46:03 +0100

    Fixed indentation problem & changed 'user' class variable to 'username'

--- ndct/core/connection.py
+++ ndct/core/connection.py
@@ -4,47 +4,47 @@ from ndct.core.device import Device
 from ndct.core.log import log
 
 class Connection(Device):
-    def __init__(self, name, ip, user, password, os):
-        '''
-	    Takes: 
-        ip:                     IPv4 address of the device
-        user:                   Username to use for device connection
-        password:               Password to use for device connection authentication
-        os:                     Operating system of the device
-	    '''
-        super().__init__(name, ip, user, password, os)
+	def __init__(self, name, ip, username, password, os):
+		'''
+		Takes: 
+		ip:                     IPv4 address of the device
+		username:               Username to use for device connection
+		password:               Password to use for device connection authentication
+		os:                     Operating system of the device
+		'''
+		super().__init__(name, ip, username, password, os)
 
-    def get_connection(self):
-        '''
+	def get_connection(self):
+		'''
 		Summary:
 		Tests device connectivity then creates an SSH connection if successful.
 
-        Returns:
-        Connection object
-	    '''
-        ping_result = ping(self.ip, count=1)
+		Returns:
+		Connection object
+		'''
+		ping_result = ping(self.ip, count=1)
 
-        if 'Request timed out' not in str(ping_result):
-	        log('{} reachable, getting connection...'.format(self.name), 'info')
+		if 'Request timed out' not in str(ping_result):
+			log('{} reachable, getting connection...'.format(self.name), 'info')
 
-            connection = Netmiko(
-		        self.ip,
-		        username=self.user,
-		        password=self.password,
-		        device_type=self.os
-            )
+			connection = Netmiko(
+				self.ip,
+				username=self.username,
+				password=self.password,
+				device_type=self.os
+			)
 
-            log('Connected to {}'.format(self.name), 'info')
-            return connection
-        else:
-	        log('{} not reachable'.format(self.name), 'info')
-            return
+			log('Connected to {}'.format(self.name), 'info')
+			return connection
+		else:
+			log('{} not reachable'.format(self.name), 'info')
+			return
 
-    def close_connection(self, connection):
-        '''
+	def close_connection(self, connection):
+		'''
 		Summary:
 		Closes an SSH connection to a device.
-	    '''
-        connection.disconnect()
+		'''
+		connection.disconnect()
 
-        log('Disconnected from {}'.format(self.name), 'info')
+		log('Disconnected from {}'.format(self.name), 'info')
