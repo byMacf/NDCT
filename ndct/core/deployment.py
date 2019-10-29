1add278cf838588bb8f7cbc13c8ce1d16bbf45d5
Dom Macfarlane <dominicmacfarlane1@hotmail.com>
2019-10-29 14:44:30 +0000

    Removed a comment

--- ndct/core/deployment.py
+++ ndct/core/deployment.py
@@ -44,7 +44,7 @@ class Deployment:
 
 		self.status = 'In progress'
 		log('Updated deployment status to In progress', 'info')
-		#Add attribute for function arg
+		
 		device_processes = [Process(target=Configuration.send_command_to_device, args=(target_device, self.action)) for target_device in self.targets]
 
 		for _process in device_processes:
