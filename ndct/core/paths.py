599bd05cabc5efd93e0f6dbff27d495f99fdc26f
Dom Macfarlane <dominicmacfarlane1@hotmail.com>
2020-03-06 00:19:43 +0000

    Fixed current_path variable that determines where paths.py variables are located

--- ndct/core/paths.py
+++ ndct/core/paths.py
@@ -1,6 +1,6 @@
 import os 
 
-current_path = os.getcwd()
+current_path = os.path.dirname(__file__)
 
 KEY_PATH = current_path + '/db/key.key'
 MODULE_PATH = os.path.dirname(current_path) + '/modules/'
