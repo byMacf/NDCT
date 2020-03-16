b6e38c9cd4637f7447000341dad5cf1ae8a55900
Dom Macfarlane <dominicmacfarlane1@hotmail.com>
2020-03-16 16:14:28 +0000

    Added alternate paths for os compatibility & testing

--- ndct/core/paths.py
+++ ndct/core/paths.py
@@ -2,9 +2,18 @@ import os
 
 current_path = os.path.dirname(__file__)
 
-KEY_PATH = current_path + '/db/key.key'
-MODULE_PATH = os.path.dirname(current_path) + '/modules/'
-CONFIG_PATH = current_path + '/configuration_files/'
-METADATA_PATH = current_path + '/device_metadata/'
-LOGGING_PATH = current_path + '/logs/'
-DB_PATH = current_path + '/db/'
+#For compatability with any operating system
+#KEY_PATH = current_path + '/db/key.key'
+#MODULE_PATH = os.path.dirname(current_path) + '/modules/'
+#CONFIG_PATH = current_path + '/configuration_files/'
+#METADATA_PATH = current_path + '/device_metadata/'
+#LOGGING_PATH = current_path + '/logs/'
+#DB_PATH = current_path + '/db/'
+
+#For testing only
+KEY_PATH = 'Documents/Python/NDCT/ndct/core/db/key.key'
+MODULE_PATH = 'Documents/Python/NDCT/ndct/modules/'
+CONFIG_PATH = 'Documents/Python/NDCT/ndct/core/configuration_files/'
+METADATA_PATH = 'Documents/Python/NDCT/ndct/core/device_metadata/'
+LOGGING_PATH = 'Documents/Python/NDCT/ndct/core/logs/'
+DB_PATH = 'Documents/Python/NDCT/ndct/core/db/'
